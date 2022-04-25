fac0a2a20b68ba9ee7639cf7a530d303a7ab8af4
Jihoi-Kang <jihoi.kang@gmail.com>
2022-04-25 20:41:14 +0900

    add test activity(move activity, view bind)

---
+++ app/src/main/java/com/project/oneco/test/TestActivity2.java
@@ -0,0 +1,16 @@
+package com.project.oneco.test;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.project.oneco.R;
+
+public class TestActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test2);
+    }
+}
