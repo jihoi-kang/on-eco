eeeb4d0a717b3a64fb4b25a4cab7773974d1000a
Soo <hiulife29@gmail.com>
2022-05-02 03:14:49 +0900

    trash 레이아웃2

---
+++ app/src/main/java/com/project/oneco/WriteTrash.java
@@ -0,0 +1,37 @@
+package com.project.oneco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class WriteTrash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_trash);
+
+
+        ImageButton Btn_back = findViewById(R.id.Btn_back);
+        Btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                onBackPressed();
+            }
+        });
+
+        ImageButton Tstatistic = findViewById(R.id.Tstatistic);
+        Tstatistic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Statistic.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
