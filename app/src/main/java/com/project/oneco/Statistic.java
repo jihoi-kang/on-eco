b999686a20fa0067f9969cfae6b8203f80c82d08
성다훈 <ekgns324@gmail.com>
2022-10-06 23:29:47 +0900

    줌

--- app/src/main/java/com/project/oneco/Statistic.java
+++ app/src/main/java/com/project/oneco/Statistic.java
@@ -323,8 +323,8 @@ public class Statistic extends AppCompatActivity {
             float val2 = trashUsage.getPlastic();
             float val3 = trashUsage.getPlastic_bag();
             float val4 = trashUsage.getCan();
-            float val5 = trashUsage.getEmpty_bottle();
-            float val6 = trashUsage.getTrashEtc();
+            float val5 = trashUsage.getGlass();
+            float val6 = trashUsage.getNormalTrash();
 
             values.add(new BarEntry(
                     i,
@@ -333,7 +333,7 @@ public class Statistic extends AppCompatActivity {
 
             dayTotalList.add(Float.toString(
                     trashUsage.getPaper() + trashUsage.getPlastic() + trashUsage.getPlastic_bag()
-                            + trashUsage.getCan() + trashUsage.getEmpty_bottle() + trashUsage.getTrashEtc()));
+                            + trashUsage.getCan() + trashUsage.getGlass() + trashUsage.getNormalTrash()));
             Log.d("hun", "dayTotal_trash : " + dayTotalList.get(i));
         }
 
@@ -432,7 +432,7 @@ public class Statistic extends AppCompatActivity {
                     ContextCompat.getColor(this, R.color.plastic),
                     ContextCompat.getColor(this, R.color.pla_bag),
                     ContextCompat.getColor(this, R.color.can),
-                    ContextCompat.getColor(this, R.color.bottle),
+                    ContextCompat.getColor(this, R.color.glass),
                     ContextCompat.getColor(this, R.color.etc)
             );
             set1.setStackLabels(new String[]{"", "", ""});
@@ -532,14 +532,14 @@ public class Statistic extends AppCompatActivity {
                     // String을 데이터 모델로 변경
                     TrashUsage today_key_trashUsage = gson.fromJson(today_key_trashUsageStr, TrashUsage.class);
                     float today_total_trash = today_key_trashUsage.getPaper() + today_key_trashUsage.getPlastic() + today_key_trashUsage.getPlastic_bag()
-                            + today_key_trashUsage.getCan() + today_key_trashUsage.getEmpty_bottle() + today_key_trashUsage.getTrashEtc();
+                            + today_key_trashUsage.getCan() + today_key_trashUsage.getGlass() + today_key_trashUsage.getNormalTrash();
                     Txt_item_all.setText("총 쓰레기 배출량 : " + today_total_trash + " g");
                     Txt_item1.setText("종이 : " + today_key_trashUsage.getPaper() + " g");
                     Txt_item2.setText("플라스틱 : " + today_key_trashUsage.getPlastic() + " g");
                     Txt_item3.setText("비닐 : " + today_key_trashUsage.getPlastic_bag() + " g");
                     Txt_item4.setText("캔 : " + today_key_trashUsage.getCan() + " g");
-                    Txt_item5.setText("공병 : " + today_key_trashUsage.getEmpty_bottle() + " g");
-                    Txt_item6.setText("기타 : " + today_key_trashUsage.getTrashEtc() + " g");
+                    Txt_item5.setText("공병 : " + today_key_trashUsage.getGlass() + " g");
+                    Txt_item6.setText("기타 : " + today_key_trashUsage.getNormalTrash() + " g");
                 }
 
             } else if (waterTypeColor_View.getVisibility() == VISIBLE) {
@@ -589,14 +589,14 @@ public class Statistic extends AppCompatActivity {
                 } else {
                     TrashUsage picked_trashUsage = gson.fromJson(picked_date_trashUsageStr, TrashUsage.class);
                     float picked_total_trash = picked_trashUsage.getPaper() + picked_trashUsage.getPlastic() + picked_trashUsage.getPlastic_bag()
-                            + picked_trashUsage.getCan() + picked_trashUsage.getEmpty_bottle() + picked_trashUsage.getTrashEtc();
+                            + picked_trashUsage.getCan() + picked_trashUsage.getGlass() + picked_trashUsage.getNormalTrash();
                     Txt_item_all.setText("총 쓰레기 배출량 : " + picked_total_trash + " g");
                     Txt_item1.setText("종이 : " + picked_trashUsage.getPaper() + " g");
                     Txt_item2.setText("플라스틱 : " + picked_trashUsage.getPlastic() + " g");
                     Txt_item3.setText("비닐 : " + picked_trashUsage.getPlastic_bag() + " g");
                     Txt_item4.setText("캔 : " + picked_trashUsage.getCan() + " g");
-                    Txt_item5.setText("공병 : " + picked_trashUsage.getEmpty_bottle() + " g");
-                    Txt_item6.setText("기타 : " + picked_trashUsage.getTrashEtc() + " g");
+                    Txt_item5.setText("공병 : " + picked_trashUsage.getGlass() + " g");
+                    Txt_item6.setText("기타 : " + picked_trashUsage.getNormalTrash() + " g");
                 }
 
             } else if (waterTypeColor_View.getVisibility() == VISIBLE) {
